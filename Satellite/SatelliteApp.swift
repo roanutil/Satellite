4c2ea36f8603606de851f9a0397bbfd7e6e1af74
Andrew Roan <roanutil@users.noreply.github.com>
2021-01-27 10:14:51 -0600

    Initial Commit

---
+++ Satellite/SatelliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliteApp.swift
+//  Satellite
+//
+//  Created by Andrew Roan on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SatelliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
